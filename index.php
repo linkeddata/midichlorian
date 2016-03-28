fe7afface47c7a07c2d7447852fa451ce931797b
rhiaro <amy@rhiaro.co.uk>
2016-03-28 11:48:32 -0400

    Post results of js request back to php to minimise repeated code

--- index.php
+++ index.php
@@ -7,20 +7,21 @@ function get($url){
   $errors = array();
   $ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$data = curl_exec($ch);
 	$ct = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
 	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	curl_close($ch);
 
-	return array('source'=>$data, 'ct'=>$ct, 'status'=>$status);
+	return array('source'=>$data, 'ct'=>$ct, 'status'=>$status, 'user'=>'None');
 }
 
-if(isset($_GET['uri']) && !empty($_GET['uri'])){
+if(isset($_POST['source']) && isset($_POST['user'])){
+  $response = $_POST;
+}elseif(isset($_GET['uri']) && !empty($_GET['uri'])){
   $response = get($_GET['uri']);
 }
-
 ?>
 
 <!doctype html>
@@ -43,6 +44,7 @@ if(isset($_GET['uri']) && !empty($_GET['uri'])){
     <?if(isset($response)):?>
       <div id="result">
         <h2>Response</h2>
+        <p>Authenticated as: <span id="user"><?=$response['user']?></span></p>
         <p>Status: <span id="status"><?=$response['status']?></span><?=($response['status'] == "401" || $response['status'] == "403") ? '<span class="noscript"> (Enable JS to authenticate and retry)</span>' : ''?></p>
         <p>Content-type: <span id="ct"><?=$response['ct']?></span></p>
         <pre id="response">
@@ -64,31 +66,49 @@ if(isset($_GET['uri']) && !empty($_GET['uri'])){
     <script>
       var solid = require('solid')
       
+      /*
+        http://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit#answer-133997
+      */
+      function post(path, params, method) {
+        method = method || "post"
+        
+        var form = document.createElement("form")
+        form.setAttribute("method", method)
+        form.setAttribute("action", path)
+    
+        for(var key in params) {
+          if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input")
+            hiddenField.setAttribute("type", "hidden")
+            hiddenField.setAttribute("name", key)
+            hiddenField.setAttribute("value", params[key])
+
+            form.appendChild(hiddenField)
+           }
+        }
+    
+        document.body.appendChild(form)
+        form.submit()
+      }
+      
       var drop = document.getElementsByClassName('noscript');
       while(drop[0]) {
         drop[0].parentNode.removeChild(drop[0]);
-      }
-      
-      var showUser = function(webId){
-        var first = document.getElementById('result').querySelector('h2')
-        var update = document.createElement('p')
-        update.innerText = 'Authenticated as ' + webId
-        first.parentNode.insertBefore(update, first.nextSibling);
       }
       
       var getResource = function(url){
         solid.web.get(url).then(
           function(response) {
             console.log(response)
-            document.getElementById('status').innerText = response.xhr.status
-            document.getElementById('ct').innerText = response.contentType()
-            document.getElementById('response').innerText = response.raw()
-            showUser(response.user)
+            var status = response.xhr.status
+            var ct = response.contentType()
+            var data = response.raw()
+            var user = response.user
+            post(window.location.href, {status: status, ct: ct, source: data, user: user})
           }
         ).catch(
           function(err) {
-            console.log(err) // error object
-            // ...
+            console.log(err)
           }
         )
       }
